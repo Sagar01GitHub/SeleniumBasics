d3388f2ee46b63ad65545375264d1ab4b065f2c8
Sagar <sagarmundale01@gmail.com>
2024-06-25 22:52:33 +0530

    dasda

---
+++ Introduction/src/Java_Automation/JavaBrushUp_StringOperation.java
@@ -0,0 +1,24 @@
+package Java_Automation;
+
+public class JavaBrushUp_StringOperation {
+
+	public static void main(String[] args) {
+
+		//String is an object and there are two ways as below:
+		
+		//1.String literal		
+		
+		String s  = "Selenium automation";
+		String s1 = "Selenium automation";
+		
+		
+		//new
+		
+		String s2 = new String("Automation");
+		String s3 = new String("Automation");
+		
+		System.out.println(s+s1+s2+s3);
+		
+	}
+
+}
